c866a6cb5a7cc3b08b121f533e4dd55539c273d1
GabrielaLucan <lucan.gabriela@ac.tuiasi.ro>
2021-04-25 18:56:12 +0300

    fix: added description on product

--- models/product.js
+++ models/product.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
+    description: String,
     imageUrl: { type: String },
     imageKey: { type: String },
     categoryId: { type: String, required: true },
