d7b3218b133625d19c02a46d03c006174ae2facd
Iuliu Visovan <iuliu.visovan@usource.ro>
2021-05-24 11:16:16 +0300

    Add Sentry on BE as well

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const expressValidator = require('express-validator');
 const passport = require('passport');
 const localStrategy = require('./services/auth/local');
 const jwtStrategy = require('./services/auth/jwt');
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+
+Sentry.init({
+  dsn: "https://d2615e583a7548a281fc04b0de577a82@o717444.ingest.sentry.io/5779948",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const app = express();
 
